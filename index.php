6483535183ebe3a76efa5f70522df5a39a6abb6b
mmahidlayon <41226855+mmahidlayon@users.noreply.github.com>
2018-08-04 13:31:16 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!doctype html>
+<html>
+<head>
+<link rel="icon" href="./img/icon.png">
+<link href="profile.css" type="text/css" rel="stylesheet">
+<title>profile</title>
+</head>
+
+<body>
+<table border="1" width=750 align="center">
+	<tr>
+	<td>
+
+		<table border="0" width=700 align="center">
+
+			<tr>
+				<td colspan="3" align="center">
+					<h2>MARLENE C. MAHIDLAYON </h2>
+					Ormoc City <br>
+					Contact # : 09104450926
+				</td>
+				<td width=150> <img src="./img/i.png" height=180 width=150>  <br></td>
+				<br> <br <br>
+			</tr>
+
+			<tr>
+				<td colspan="4"> <br></td>
+			</tr>
+
+			<tr>
+				<td> <strong> Age: </strong> </th>
+				<td> 23</td>
+				<td> <strong> Citizenship: </strong> </th>
+				<td> Filipino</td>
+				</tr>
+
+			<tr>
+				<td> <b> Status: </b> </th>
+				<td> ACLC College</td>
+				<td> <b> Status: </b> </th>
+				<td> Single</td>
+			</tr>
+
+			<tr>
+				<td> <b> Height: </b></th>
+				<td> 149 cm</td>
+				<td> <b> Religion: </b> </th>
+				<td> Roman Catholic</td>
+			</tr>
+
+			<tr>
+				<td> <b> Weight: </b> </th>
+				<td> 45 kgs.</td>
+				<td> <b> Blood Type:  </b></th>
+				<td> B</td>
+			</tr>
+
+			<tr>
+				<td colspan="4"> <br></td>
+			</tr>
+
+			<tr>
+				<td> <strong>  Objective: </strong> </td>
+				<td colspan="3"> To acquire a job that would fit my specialty and makes use of the skills I have for 									the benefit of the institution and its stakeholders.
+                </td>
+			</tr>
+
+			<tr>
+				<td colspan="4" align="center"> <h3> EDUCATION </h3></td>
+			</tr>
+
+			<tr>
+				<td colspan="4"> 
+
+<!-- using unordered list -->
+					<ul>
+						<li> <b> Bachelor of Science in Information Technology </b><br>
+								ACLC College of Ormoc <br>
+								Ormoc City, Leyte <br>
+								June 2011- April 25, 2015
+						</li>
+						<li> <b> Secondary </b><br>
+								 Kananga National High School <br>
+								 Kananga, Leyte <br>
+								 June 2007- March, 2011
+						</li>
+						<li>
+							<b>Primary </b><br>
+								Rizal Elementary School <br>
+								Brgy. Rizal, Kananga, Leyte <br>
+								June 2001- March, 2017 
+						</li>
+				</td> 
+			</tr>
+
+			<tr>
+				<td colspan="4" align="center"> <h3> EXPERIENCE </h3></td>
+			</tr>
+
+			<tr>
+				<td colspan="4"> <strong>  Faculty  </strong><br>
+											IT Department, ACLC College of Ormoc <br>
+											Ormoc City, Leyte <br>
+										    November, 2015-present
+				</td> 
+			</tr>
+
+			<tr>
+				<td colspan="4">
+
+				</td>
+			</tr>
+
+			<tr>
+				<td colspan="4"> <em> <b> About Self: </b> </em> <br></td>
+			</tr>
+
+			<tr>
+				<td colspan="4">
+					<p>
+                    <i>
+						I am a shy type person but when you get to know me, well, I am fun to be with. 
+						I love music very much. When I am down, I just read quotes and create my own. I love 										                        cheering others and importantly, I fear God more than everyone else.
+                        </i>
+					</p>
+				</td>
+			</tr>
+
+	</table>
+
+
+</td>
+</tr>
+</table>
+
+
+</body>
+</html>
